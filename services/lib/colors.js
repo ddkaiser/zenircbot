1babe5e95aae9c0f8ebf4f03f9a4be980d876e6d
David Kaiser <dkaiser@cdk.com>
2013-02-18 11:53:29 -0800

    adding colors from mIRC online doc - need more thorough testing

--- services/lib/colors.js
+++ services/lib/colors.js
@@ -1,6 +1,21 @@
 colors = {
+    white: '\u000300',
+    black: '\u000301',
+    navy:  '\u000302',
     green: '\u000303',
-    reset: '\u000f',
-};
+    red:   '\u000304',
+    brown: '\u000305',
+    purple: '\u000306',
+    olive: '\u000307',
+    yellow: '\u000308',
+    lightgreen: '\u000309',
+    teal: '\u000310',
+    cyan: '\u000311',
+    blue: '\u000312',
+    pink: '\u000313',
+    gray: '\u000314',
+    lightgray: '\u000315',
+    reset: '\u000f'
+}
 
 module.exports = colors;
